35d8cbb5818810145454c601e29effd965232efd
naresh <naresh@gmail.com>
2023-06-06 17:38:16 +0530

    tss

---
+++ task/Readme.md
@@ -0,0 +1,20 @@
+Before running the application, create the database
+
+Note: Run in local
+        npm i
+        node index.js
+
+1. Create Database students_records
+2. Create Table name students and column [ id INT, name VARCHAR(255), course VARCHAR(255) ]
+3. Insert values into the students table Eg: (id, name, course) (1, 'John Doe', 'Computer Science')
+
+Task 1: 
+Run the application in local and conect with mysql.
+
+Task 2:
+Run the application in the docker and the database should also be in docker container with docker compose.
+
+Task 3:
+Run the application in docker without docker compose and Database should be in local system.
+
+
---
+++ task/index.js
@@ -0,0 +1,28 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "students_records",
+});
+
+connection.connect((err) => {
+    if (err) {
+      console.log("Error occurred", err);
+    } else {
+      console.log("Connected to database");
+    //   var sql = "INSERT INTO students (id, name, course) VALUES (1, 'John Doe', 'Computer Science')";
+      connection.query(sql, function (err, result) {
+        if (err) {
+          console.log(err);
+        }
+        // console.log("Value inserted");
+      });
+    }
+});
+
+var sql = "SELECT * FROM students";
+connection.query(sql, function (err, result) {
+  console.log(result);
+});
